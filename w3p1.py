a69b16de06b81a4860772b898c52f1f5bfb3baef
Vince <vhorvath2010@gmail.com>
2022-07-15 14:06:00 -0400

    Initial commit

---
+++ w3p1.py
@@ -0,0 +1,7 @@
+import requests
+
+print("Version:", requests.__version__)
+print("License:", requests.__license__)
+print("Copyright:", requests.__copyright__)
+print("Title:", requests.__title__)
+print("Build:", requests.__build__)
